e3158f87b4b181f72892398690016400acfbc851
sinusamm <eesti.mail@mail.ru>
2021-08-26 20:27:53 +0300

    Peremennie i uslovnie operatori

---
+++ src/com/company/Main2.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main2 {
+    public static void main(String[] args) {
+    int num1 = 10;
+    int num2 = 15;
+
+    if(num2>num1){
+       System.out.println("num 2 is bigger than num1");
+
+    } else{
+        System.out.println("num1 is bigger than num2");
+
+    }if (num2==20) {
+            System.out.println("num23 is 20");
+        }else{
+        System.out.print(" num2 is not 20");
+        }
+//    if(num2!=20) {
+//        System.out.println("num2 is not 20");
+//    }else{
+//        System.out.print("num2 is  20");
+//    }
+    }
+}
+
