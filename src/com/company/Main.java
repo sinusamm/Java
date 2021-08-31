0b720a5fe31402f3f06a49c28ef74333a830e687
sinusamm <eesti.mail@mail.ru>
2021-08-31 17:57:09 +0300

    praktika1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int num1= 5;
-        int num2= 10;
-        int result= num1+ num2;
-        System.out.println(result);
-
-
-        System.out.print("Hello world!");
-        int num3, num4, result2;
-        num3=5;
-        num4=10;
-        result2 = num3 + num4;
-        // result++; uvelichivaet na 1
-        System.out.print(result2);
+	// write your code here
     }
 }
