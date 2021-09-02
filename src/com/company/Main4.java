3b468cb0641be9f37db8d4e159679a4f4ad30f10
sinusamm <eesti.mail@mail.ru>
2021-09-02 20:04:09 +0300

    If-Else if-else!

---
+++ src/com/company/Main4.java
@@ -0,0 +1,16 @@
+package com.company;
+public class Main4 {
+    public static void main(String[] args) {
+        int num1, num2, num3;
+        num1 = 5;
+        num2 = 10;
+        num3 = num1+num2;
+        if(num3 == 15){
+            System.out.print("num3 is 15");
+        }else if (num3 != 16){
+            System.out.print("num1 is 5");
+        } else {
+            System.out.print("num3 is some num");
+        }
+    }
+}
