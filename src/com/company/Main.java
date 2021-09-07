4dad765cf513aad4aa04d52336cb66c57259c912
Sinusamm <eesti.mail@mail.ru>
2021-09-07 15:49:54 +0300

    golova rabotai

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,3 @@
 package com.company;
 
-import java.util.Scanner;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        Scanner num = new Scanner(System.in);
-        int = float
-        System.out.print("Enter float number");
-        float = num.nextInt();
-        System.out.print("float");
-
-    }
-
-
-}
+//ne sidi dolgo
--- src/com/company/Main4.java
+++ src/com/company/Main4.java
@@ -1,16 +1,18 @@
 package com.company;
+
 public class Main4 {
-    public static void main(String[] args) {
-        int num1, num2, num3;
-        num1 = 5;
-        num2 = 10;
-        num3 = num1+num2;
-        if(num3 == 15){
-            System.out.print("num3 is 15");
-        }else if (num3 != 16){
-            System.out.print("num1 is 5");
-        } else {
-            System.out.print("num3 is some num");
+    public static void main(String[] args){
+    int num1, num2, num3;
+        num1=5;
+        num2=10;
+        num3=num1+num2;
+        if(num3<10){
+            System.out.println("num 3 is less 10");
+
+        }else if(num3==15) {
+            System.out.println("num3 is  15");
+
+        }else{
+            System.out.print("num3 is bigger than 10");
         }
     }
-}
