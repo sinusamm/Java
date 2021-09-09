aaede5a716addc503d9ae086b7cec05e205eb4d1
sinusamm <eesti.mail@mail.ru>
2021-09-09 20:13:17 +0300

    While

---
+++ src/com/company/While.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class While {
+    public static void main(String[] args) {
+        // rabotaet poka znach.yavl istinnim ili neistinnm to est true ili false
+        boolean isTrue=true;
+        // boolean libo true libo false dr.znachenie ne bivaet
+        int j=0;
+        while(isTrue) {
+            System.out.println("hello!");
+            //System.out.print("hello!");---vivod v konsol
+            j++;
+            if(j>5){
+                break;
+                // mojno break zamenit isTrue=false;
+                // tolko v while tsikle stavitsya break
+                //budet vivoditsya 6 raz tak kak j=o
+
+
+
+            }
+
+        }
+
+    }
+}
