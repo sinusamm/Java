abb62fb7d6afc0a4c99319a89417b909590690db
sinusamm <eesti.mail@mail.ru>
2021-09-13 19:33:44 +0300

    Zadanie na While

---
+++ src/com/company/While_praktika.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class While_praktika {
+    public static void main(String[] args) {
+//        boolean status = true;
+//        int num1=0;
+//        while(status){
+//            num1++;
+//            if(num1 > 10){
+//                status=false;
+//            }
+//            System.out.println(status);
+            // budet vihodit 9 raz true i potom false
+        int num1=0;
+        while(num1<100){
+            num1+=5;
+            System.out.print(num1+ " ");
+            // 0+5 menshe 100...tsikl rab-----5+5 ---i tak do 100
+            // delai to poka while rabotaet
+
+}
+
+    }
+}
