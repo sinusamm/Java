24066d8a314341f7f91c33a7f672a95e389e1911
sinusamm <eesti.mail@mail.ru>
2021-09-13 18:10:43 +0300

    Zadanie na FOR

---
+++ src/com/company/Praktika.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Praktika {
+    public static void main(String[] args) {
+//        for(int i=1; i<6; i++){
+//            System.out.print(i +" ");
+            // (" ") eto oznachaet probel 1 2 3 4 5
+//            System.out.print("\n");
+            // ("\n")---toshe samoe kak ln
+//        for(int i = 5; i>=1; i--){
+//            System.out.print(i);
+//        for(int i=0; i<5; i++){
+//            System.out.print("*");
+//        for(int i=0; i<3; i++){
+            // eto komanda oznachaet skolko raz budet srabativaet  vtoroi tsikl; on budet rabotat
+//            for(int j=0; j<5; j++){
+//                System.out.print("* ");
+                // a eto o tom skolko  zvezd budet vivoditsya
+//            }
+//            System.out.print("\n");
+        for(int i =0; i<2; i++){
+            //skolko raz budet i pervi for i vtoroi for
+            for(int j=0; j<3; j++){
+                //ryadi
+                for(int k=0; k<4; k++){
+                    // zvezd
+                    System.out.print("* ");
+            }
+                System.out.print("\n");
+        }
+            System.out.print("\n");
+        }
+    }
+}
+
+
