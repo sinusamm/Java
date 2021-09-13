da755a72d57843f36adea353b320dc1787d07440
sinusamm <eesti.mail@mail.ru>
2021-09-13 19:56:17 +0300

    Do while

---
+++ src/com/company/Do_While.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Do_While {
+    public static void main(String[] args) {
+//        while(true){
+//        }
+//        int num1=5;
+//        do{
+//            num1++;
+//            System.out.print(num1+ " ");
+//        }while(num1>10);
+        // v nachale delaet potom idet proverka na istinnom...chislo nashe bolshe 10 ili net
+        // 6 nikak ne mojet bit bolshe 10 i poetomu proverka ne proshla
+         int num1= -5;
+        do{
+            num1++;
+            System.out.print("vvedite pojaluista polojitelnoe chislo ");
+    }while(num1>10);
+    }
+}
