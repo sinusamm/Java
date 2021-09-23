98975760f93b49ee6cb1347fa7a0994de63e864e
Sinusamm <eesti.mail@mail.ru>
2021-09-23 16:08:53 +0300

    dom_zadanie

--- src/com/company/Dom_zadanie.java
+++ src/com/company/Dom_zadanie.java
@@ -1,7 +1,7 @@
 package com.company;
 
 import java.util.Scanner;
-
+import java.util.Random;
 public class Dom_zadanie {
     public static void main(String[] args) {
     Scanner number2= new Scanner(System.in);
@@ -10,7 +10,10 @@ public class Dom_zadanie {
     int num1, num2,result;
             System.out.print("First number is secret, please enter second number: ");
     num2 = number2.nextInt();
- num1= 88;
+        Random rand= new Random();
+        for(int i=0; i<5; i++) {
+    num1 = rand.nextInt();
+    System.out.println(num1);
  // num1---zarandomit
     result = num1 + num2;
             if(result<100){
@@ -18,6 +21,6 @@ public class Dom_zadanie {
     }else {
         System.out.print( "Game over!, because result was bigger then 100!");
     }
-
+        }
 }
 }
