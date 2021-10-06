0bf0b8cd308d4b03929787acc26ffa4555596075
Sinusamm <eesti.mail@mail.ru>
2021-10-06 20:30:33 +0300

    Summa_chisel_iz_massiva

--- src/com/company/Main18_zadanie.java
+++ src/com/company/Main18_zadanie.java
@@ -3,5 +3,12 @@ package com.company;
 public class Main18_zadanie {
     public static void main(String[] args) {
         int [] array= new int[] {50,60,50,70,10,5,100};
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        System.out.println(sum);
     }
-}
+    }
+
+
